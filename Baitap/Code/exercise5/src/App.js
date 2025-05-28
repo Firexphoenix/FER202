2e1a8733befbcb45ed93f3a6647514a033a0a18e
Lê Vũ Minh Hoàng <levuminhhoang11112004@gmail.com>
2025-05-28 14:59:29 +0700

    Exercise 5

---
+++ Baitap/Code/exercise5/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Exercise3 from './Exercise3';
+import Exercise4 from './Exercise4';
+import Exercise5 from './Exercise5';
+
+function App() {
+  return (
+    // <Exercise3/>
+    // <Exercise4/>
+    <Exercise5/>
+  );
+}
+
+export default App;
